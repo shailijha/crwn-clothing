1f2a2c70832d778edb4adca6372508ced7eacf71
shailijha <shaili_jha@yahoo.co.in>
2020-05-16 18:55:58 -0500

    stored user details in state

--- src/App.js
+++ src/App.js
@@ -21,10 +21,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      //console.log(this.state);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state)
+          })
+        })
+      }
+      else {
+        this.setState({ currentUser: userAuth });
+      }
     })
   }
 
