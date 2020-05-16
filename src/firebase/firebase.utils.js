cb08596ace0892cee53f900810509004f0991281
shailijha <shaili_jha@yahoo.co.in>
2020-05-16 17:35:13 -0500

    added create user profile document function

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -22,4 +22,32 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    console.log(snapShot.exists);
+
+    if(!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating a user', error.message)
+        }
+    }
+
+    return userRef;
+}
+
 export default firebase;
